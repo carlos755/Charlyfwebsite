ab9d6859626914afe268e32844e0d80ee267f706
carlos755 <75638202+carlos755@users.noreply.github.com>
2023-01-15 11:47:27 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ const body = document.querySelector("body"),
     modeToggle = document.querySelector(".dark-light"),
     searchToggle = document.querySelector(".searchToggle");
 
-// js code to toggle dark and light mode 
     modeToggle.addEventListener("click"  , ()  =>{
         modeToggle.classList.toggle("active");
         body.classList.toggle("dark");
     });
+
+var player = document.getElementById('player');
+var songs = [
+  'audio/Chalino.mp3',
+  'audio/Zutano (En Vivo).mp3',
+  'audio/El Mochitense.mp3',
+];
+var currentIndex = 0;
+
+player.src = songs[currentIndex];
+player.play();
+
+player.addEventListener('ended', function() {
+  currentIndex++;
+  if (currentIndex === songs.length) {
+    currentIndex = 0;
+  }
+  player.src = songs[currentIndex];
+  player.play();
+});
