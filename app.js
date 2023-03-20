e8a9dd2b6041ed65d2ccde66d455802f7186ef44
carlos755 <75638202+carlos755@users.noreply.github.com>
2023-03-19 18:42:14 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -10,4 +10,28 @@ window.addEventListener('load', function() {
     var loader = document.getElementById('loader-container');
     loader.style.display = 'none';
   });
+  var canciones = ["cancion1", "cancion2", "cancion3"];
+  var indiceActual = 0;
+  cancion1.volume = 0.2;
+  cancion2.volume = 0.2;
+  cancion3.volume = 0.2;
+  var audio = document.getElementById(canciones[indiceActual]);
   
+  function playAudio() {
+    audio.play();
+  }
+  
+  function pauseAudio() {
+    audio.pause();
+  }
+  
+  function nextAudio() {
+    indiceActual++;
+    if (indiceActual >= canciones.length) {
+      indiceActual = 0;
+    }
+    audio.pause();
+    audio = document.getElementById(canciones[indiceActual]);
+    audio.currentTime = 0;
+    audio.play();
+  }
